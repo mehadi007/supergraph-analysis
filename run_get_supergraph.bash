272bf60162fcda8d46b99735ad43f89862033546
me <yikeliu@umich.edu>
2016-06-10 22:24:23 -0400

    marg method added

--- run_get_supergraph.bash
+++ run_get_supergraph.bash
@@ -3,7 +3,7 @@
 # This files generate model analysis and supergraphs
 # input:
 #   data: chocolate, oregon, caida, enron, euemail
-#   method: vog, gnf, step, stepa, kstep
+#   method: vog, gnf, marg, step, stepa, kstep
 
 data=$1
 method=$2
@@ -19,6 +19,7 @@ esac)
 targetFolder=$(case "$method" in
     (vog) echo vog_orig_results;;
     (gnf) echo gnf_results;;
+    (marg) echo marg_results;;
     (step) echo parallel_results/plain;;
     (stepa) echo parallel_results/active;;
     (kstep) echo parallel_results/topK
@@ -26,8 +27,11 @@ esac)
 
 if [ "$method" = "vog" ] || [ "$method" = "gnf" ]; then
     python2.7 parse_graph.py DATA_FOR_SUPERGRAPH/graphs/$filename.graph DATA_FOR_SUPERGRAPH/$targetFolder/$filename\_orderedALL.model DATA_FOR_SUPERGRAPH/$targetFolder DATA_FOR_SUPERGRAPH/$targetFolder/heuristicSelection_nStop_ALL_$filename\_orderedALL.model DATA_FOR_SUPERGRAPH/$targetFolder/heuristic_Selection_costs_ALL_$filename\_orderedALL.model > DATA_FOR_SUPERGRAPH/$targetFolder/OUTPUT_$filename.txt &
+elif [ "$method" = "marg" ]; then
+    python2.7 parse_graph.py DATA_FOR_SUPERGRAPH/graphs/$filename.graph DATA_FOR_SUPERGRAPH/$targetFolder/$filename\_orderedALL.model DATA_FOR_SUPERGRAPH/$targetFolder DATA_FOR_SUPERGRAPH/$targetFolder/greedySelectionMargin_nStop_$filename\_orderedALL.model DATA_FOR_SUPERGRAPH/$targetFolder/greedySelectionMargin_costs_$filename\_orderedALL.model > DATA_FOR_SUPERGRAPH/$targetFolder/OUTPUT_$filename.txt &
 elif [ "$method" = "step" ] || [ "$method" = "stepa" ] || [ "$method" = "kstep" ]; then
-    python2.7 parse_graph.py DATA_FOR_SUPERGRAPH/graphs/$filename.graph DATA_FOR_SUPERGRAPH/$targetFolder/$filename\_structures_1 DATA_FOR_SUPERGRAPH/$targetFolder DATA_FOR_SUPERGRAPH/$targetFolder/$filename\_structures_1 DATA_FOR_SUPERGRAPH/$targetFolder/$filename\_encoding_1 > DATA_FOR_SUPERGRAPH/$targetFolder/OUTPUT_$filename.txt &
+    #python2.7 parse_graph.py DATA_FOR_SUPERGRAPH/graphs/$filename.graph DATA_FOR_SUPERGRAPH/$targetFolder/$filename\_orderedALL.model DATA_FOR_SUPERGRAPH/$targetFolder DATA_FOR_SUPERGRAPH/$targetFolder/$filename\_structures_1 DATA_FOR_SUPERGRAPH/$targetFolder/$filename\_encoding_1 > DATA_FOR_SUPERGRAPH/$targetFolder/OUTPUT_$filename.txt &
+    python2.7 parse_graph.py DATA_FOR_SUPERGRAPH/graphs/$filename.graph DATA_FOR_SUPERGRAPH/$targetFolder/$filename\_orderedALL.model DATA_FOR_SUPERGRAPH/$targetFolder DATA_FOR_SUPERGRAPH/$targetFolder/$filename\_structures_1 > DATA_FOR_SUPERGRAPH/$targetFolder/OUTPUT_$filename.txt &
 else
     echo 'Wrong input.'
 fi
